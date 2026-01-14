454b5291c9148d3d94a02d3d37bb148aa5dd7d2d
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2026-01-14 12:37:55 -0800

    update deploy settings

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,12 +1,14 @@
+require('dotenv').config();
+
 module.exports = {
   apps: [{
     name: 'polymarket-combination',
     script: 'node_modules/next/dist/bin/next',
-    args: 'start',
+    args: `start -p ${process.env.PORT || 3000}`,
     interpreter: 'bun',
-    env_file: '.env',
     env: {
-      NODE_ENV: 'production'
+      NODE_ENV: 'production',
+      ...require('dotenv').config().parsed
     },
     instances: 1,
     autorestart: true,
